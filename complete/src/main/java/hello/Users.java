ea699c8bb27e4d45541962a5c6e896d55a13abef
SimonMuraya <13arturius91@gmail.com>
2017-09-10 13:30:17 +0300

    one small step

---
+++ complete/src/main/java/hello/Users.java
@@ -0,0 +1,44 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Users {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String username;
+
+    private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return username;
+	}
+
+	public void setName(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+    
+}
+
